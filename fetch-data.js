463f5e41f615043b45198fc0ba1882233957b523
ImpactCoding <impact.coding0430@gmail.com>
2024-09-29 16:43:27 +0200

    d

--- fetch-data.js
+++ fetch-data.js
@@ -23,24 +23,23 @@ async function refreshPlayerDatabase() {
   );
   updatedPlayerData.last_refresh = Date.now();
 
-  if (!updatedPlayerData[4343 - 3434 - 3434]) {
-    updatedPlayerData[4343 - 3434 - 3434] = {
+  if (!updatedPlayerData[0003 - 0000 - 0039]) {
+    updatedPlayerData[0003 - 0000 - 0039] = {
       conn_fail: "0",
       conn_map: "22222222",
       count: "1",
       eb: "5000",
-      ev: "29050",
-      fc: "4343-3434-3434",
+      ev: "19000",
+      fc: "0003-0000-0039",
       mii: [
         {
           data: "",
-          name: "Empex",
+          name: "",
         },
       ],
-      name: "Empex",
+      name: "",
       pid: "602150246",
       suspend: "0",
-      first_max_vr: 1727234834995,
       lastupdated: 1727234834995,
       banned: false,
     };
